2d76fe17a5d0371f06c41a14ef3d1e00579599e3
Hugo Medeiros <hugomedeiroslm@gmail.com>
2024-10-02 13:12:46 -0300

    Primeiro commit

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Olá Vue!</h1>
+        <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+        <MultiplosEventos />
+        <Reutilizacao />
+        <Reutilizacao />
+        <Reutilizacao />
+    </div>
+</template>
+
+<script>
+    import MultiplosEventos from './MultiplosEventos.vue'
+    import Reutilizacao from './Reutilizacao.vue';
+
+    export default {
+        name: 'PrimeiroComponente',
+        components: {
+            MultiplosEventos,
+            Reutilizacao
+        },
+        data(){
+            return{
+                nome: "Hugo",
+                profissao: "Programador"
+            }
+        }
+    }
+</script>
